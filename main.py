32a1107cbaaf1965d1afbd14c16ecdf402ff96a7
bhavi8014 <gandhibhavi162@gmail.com>
2025-05-13 20:34:19 +0530

    bill

---
+++ main.py
@@ -0,0 +1,15 @@
+unit = int(input("enter your unit: "))
+if unit < 50:
+    amount = unit*2.60
+    tax = 25
+elif unit <= 100:
+    amount = 130+((unit-50)*3.25)
+    tax = 35
+elif unit <= 200:
+    amount = 130+162.50+((unit-100)*5.26)
+    tax = 45
+else:
+    amount = 130+162.50+526+((unit-200)*8.45)
+    tax = 75
+total = amount+tax
+print(unit , total)
